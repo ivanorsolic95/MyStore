e8263a241880459740a85d3b2565f94dc0593ce8
Ivan Orsolic <orsolic1995@gmail.com>
2024-05-03 15:55:44 +0200

    Refactor the confirmation service

--- src/service/confirmation.service.ts
+++ src/service/confirmation.service.ts
@@ -5,12 +5,56 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class ConfirmationService {
-  private _fullName = new BehaviorSubject<string>('');
-  fullName$ = this._fullName.asObservable();
+  private Name = new BehaviorSubject<string>('');
+  fullName$ = this.Name.asObservable();
+  
+  private LastName = new BehaviorSubject<string>('');
+  lastName$ = this.LastName.asObservable()
+
+  private Address = new BehaviorSubject<string>('');
+  address$ = this.Address.asObservable()
+
+  private PostalCode = new BehaviorSubject<string>('');
+  postalCode$ = this.PostalCode.asObservable()
+
+  private City = new BehaviorSubject<string>('');
+  city$ = this.City.asObservable()
+
+  private PaymentMethod = new BehaviorSubject<string>('');
+  paymentMethod$ = this.PaymentMethod.asObservable()
+
+  private CreditCard = new BehaviorSubject<string>('');
+  creditCard$ = this.CreditCard.asObservable()
+
 
   constructor() { }
 
-  setFullName(name: string): void {
-    this._fullName.next(name);
+  setName(name: string): void {
+    this.Name.next(name);
   }
+
+  setLastName(lastName: string): void {
+    this.LastName.next(lastName);
+  }
+
+  setAddress(address: string) : void {
+    this.Address.next(address);
+  }
+
+  setPostalCode(postalCode: string) : void {
+    this.PostalCode.next(postalCode);
+  }
+
+  setCity(city: string) : void {
+    this.City.next(city);
+  }
+
+  setPaymentMethod(paymentMethod: string) : void {
+    this.PaymentMethod.next(paymentMethod);
+  }
+  
+  setCreditCard(creditCard: string) : void {
+    this.CreditCard.next(creditCard);
+  }
+
 }
