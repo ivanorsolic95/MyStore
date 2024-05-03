8f0aeb6ebdf61aa747cde14046709462476d34b7
Ivan Orsolic <orsolic1995@gmail.com>
2024-05-03 15:53:41 +0200

    Add new properties to the ConfirmationMessage component

--- src/app/confirmation-message/confirmation-message.component.ts
+++ src/app/confirmation-message/confirmation-message.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfirmationService } from 'src/service/confirmation.service';
-import { Router } from '@angular/router';
+import { CartService } from 'src/service/cart.service';
+import { Router} from '@angular/router';
+import { CartItem } from 'src/models/CartItem';
 
 @Component({
   selector: 'app-confirmation-message',
@@ -8,16 +10,43 @@ import { Router } from '@angular/router';
   styleUrls: ['./confirmation-message.component.css']
 })
 export class ConfirmationMessageComponent implements OnInit {
-  fullName: string = '';
+  cartItems: CartItem[] = []
+  name: string = '';
+  lastName: string = '';
+  address:string = '';
+  postalCode:string = '';
+  city:string = '';
+  paymentMethod:string = '';
+  creditCard:string = '';
 
   constructor(private confirmationService: ConfirmationService,
-                      private router: Router) {}
+              private cartService: CartService,
+              private router: Router) {}
 
   ngOnInit(): void {
-    this.confirmationService.fullName$.subscribe(name => this.fullName = name);
+    this.cartService.cart.subscribe(items => this.cartItems = items)
+    this.confirmationService.fullName$.subscribe(name => this.name = name);
+    this.confirmationService.lastName$.subscribe(lastName => this.lastName = lastName);
+    this.confirmationService.address$.subscribe(address => this.address = address);
+    this.confirmationService.postalCode$.subscribe(postalCode => this.postalCode = postalCode);
+    this.confirmationService.city$.subscribe(city => this.city = city);
+    this.confirmationService.paymentMethod$.subscribe(paymentMethod => this.paymentMethod = paymentMethod);
+    this.confirmationService.creditCard$.subscribe(creditCard => this.creditCard = creditCard);
   }
 
   goToProductList () {
     this.router.navigate(['/']);
   }
+
+  calculateTotalCost(): string {
+    let total = this.cartItems.reduce((acc, item) => acc + item.totalPrice, 0);
+    return total.toFixed(2);
+  }
+
+  additionalcost(): number {
+    let baseCost = parseFloat(this.calculateTotalCost());
+    let additionalCost = baseCost + 5;
+    return parseFloat(additionalCost.toFixed(2));
+  }
+
 }
